080d755cf511d4a768ec86401833a96ad81eed46
Sven Labtop <sven.gessner@yahoo.de>
2021-10-16 01:27:08 +0200

    Finally the website has a Theme and content. Added About and Contact site.

--- javascript/index.js
+++ javascript/index.js
@@ -1,7 +1,3 @@
-setTimeout(() => {
-  document.getElementById('splash').classList.add('fade');
-}, 750);
-
 const toggleButton = document.getElementsByClassName('toggle-button')[0]
 const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 
